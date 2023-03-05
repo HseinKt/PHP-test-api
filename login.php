657d8672e3328f55e1b287431c5908d6e9905069
HseinKt <hseinkteish1998@gmail.com>
2023-03-06 01:32:24 +0200

    added password validation in the login php file

--- login.php
+++ login.php
@@ -17,8 +17,24 @@ else if (preg_match($Email_pattern,$email)){
     $emailResult = "Email Failed";
 }
 
+// Password Validate
+if (empty($password)){
+    $passwordResult = "password is required";
+} 
+else if(strlen($password) < 8){
+    $passwordResult = " Password should contain 8 characters minimum";
+}
+else if (!preg_match($specialChars,$password)){
+    $passwordResult = "Password should contain one special character at least";
+}
+else if (!preg_match($upperCase,$password)){
+    $passwordResult = "Password should contain at least one upper case letter";
+}
+else {
+    $passwordResult = "Password success";
+}
 
 
 
-echo json_encode( $emailResult);
+echo json_encode( $passwordResult);
 ?>
