83a877008e9bc507d9a9759ace85358bda62fcc0
HseinKt <hseinkteish1998@gmail.com>
2023-03-06 02:49:08 +0200

    added a GET API that returns your current location.

--- get_IP_addr.php
+++ get_IP_addr.php
@@ -10,5 +10,15 @@ else {
     $ipResult = "UNKNOWN";
 }
 
-echo json_encode( $ipResult);
+// echo json_encode( $ipResult);
+
+$query = @unserialize (file_get_contents('http://ip-api.com/php/'));
+    if ($query && $query['status'] == 'success') {
+        echo 'Hey user from ' . $query['country'] . ', ' . $query['city'] . '!';
+    }
+    foreach ($query as $data) {
+        echo $data . "<br>";
+    }
+
+echo json_encode( $query);
 ?>
