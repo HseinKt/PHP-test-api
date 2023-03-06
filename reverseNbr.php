6bf86d541b39b2977eb5fcb9b793e9f0f8749c33
HseinKt <hseinkteish1998@gmail.com>
2023-03-06 03:38:27 +0200

    updated

--- reverseNbr.php
+++ reverseNbr.php
@@ -2,21 +2,18 @@
 
 // A GET API that takes a string and returns a new string with reversed numbers in that string.The string might not contain numbers. 
 
-$string = $_POST['string'];
+$string = $_GET['string'];
 $numbers = "/[0-9]/";
 
 if (empty($string)){
     $stringResult = "string is required";
 } 
-else if (!preg_match($numbers,$string)){
-    $stringResult = $string;
-}
-else {
+else if (preg_match($numbers,$string)){
     preg_match_all($numbers, $string, $matches);
     $arr = $matches[0];
     $n = count($arr);
     $j=0;
-    for ($i = $n ; $n > 0 ; $i-- ) {
+    for ($i = $n ; $i > 0 ; $i-- ) {
         $temp = $arr[$j];
         $arr[$j] = $arr[$i];
         $arr[$i] = $temp;
@@ -24,6 +21,9 @@ else {
     }
     $stringResult = implode($arr);
 }
-
+else {
+    $stringResult = $string;
+}
+    
 echo json_encode( $stringResult);
 ?>
